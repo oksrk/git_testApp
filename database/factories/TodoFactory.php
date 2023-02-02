4b0339d871245ab927a5a8694d5cabfce591c976
oksrk <124051257+oksrk@users.noreply.github.com>
2023-02-02 12:36:11 +0900

    Add files via upload

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Todo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+/**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Todo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        // $title = substr(str_shuffle("ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz0123456789"), 0, 5);
+        // $content = substr(str_shuffle("ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz0123456789"), 0, 5);
+        
+        // Todo::factory()->count(5)->make([
+        //     'title' => substr(str_shuffle("ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz0123456789"), 0, 5),
+        //     'content' => substr(str_shuffle("ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz0123456789"), 0, 5)
+        // ]);
+        // Todo::factory()->count(5)-> create();
+
+        return [
+            'title' => $this->faker->name,
+            'content' => $this->faker->name
+        ];
+    }
+}
+
